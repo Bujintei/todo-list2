815d97f125acc213d60696ef2f2b789feb321798
Brandon Nguyen <eagleeye2555@gmail.com>
2015-04-23 11:26:14 -0700

    Video 17, 18, and 19 of our 2nd To-Do App

--- index.php
+++ index.php
@@ -19,8 +19,8 @@
 							$task_name = $row['task'];
 
 							echo '<li> 
-							<span>' . $task_name . '</span>
-							<img id="' . $task_id . '" class="delete-button" width="10px" src="images/close.svg"/>
+							<span>'. $task_name . '</span>
+							<img id="'. $task_id . '" class="delete-button" width="10px" src="images/close.svg"/>
 							</li>';
 						}
 					}
@@ -35,7 +35,7 @@
 		</form>
 	</div>
 </body>
-<script src="http://code.jqeury.com/jquery-latest.min.js"></script>
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
 <script>
 
 	add_task(); //calling the add task function
@@ -53,12 +53,11 @@
 			return false;
 		});
 	}
-
 	$('.delete-button').click(function(){
 		var current_element = $(this);
 		var task_id = $(this).attr('id');
 		
-		$.post('inludes/delete-task.php', {id: task_id}, function(){
+		$.post('includes/delete-task.php', {id: task_id}, function(){
 			current_element.parent().fadeOut("fast", function(){
 			$(this).remove();
 			});
